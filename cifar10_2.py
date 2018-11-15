f285039848dd54357d30d8481c0f7cb12ca19c69
aiinside-6 <tripdancer0916@gmail.com>
2018-11-15 12:53:31 +0900

    debug?

--- cifar10_2.py
+++ cifar10_2.py
@@ -82,7 +82,7 @@ import torch.optim as optim
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(1):  # loop over the dataset multiple times
+for epoch in range(10):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
